21739778d2b6b67fa88d6298b69b1c03adce6d0f
Brian Pacia <brianjeffreyppacia@yahoo.com>
2019-02-23 16:40:27 +0800

    added habit files

--- Bibendum/app/src/main/java/com/bibendum/bluehacks/bibendum/MainActivity.java
+++ Bibendum/app/src/main/java/com/bibendum/bluehacks/bibendum/MainActivity.java
@@ -1,7 +1,9 @@
 package com.bibendum.bluehacks.bibendum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //To make the back button at the tab
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent ag = new Intent(getApplicationContext(), HabitActivity.class);
+                startActivity(ag);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
